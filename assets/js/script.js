b6e814ce728444aab05c319ec277d727ae87d0b8
Deh Saaduddin <dehgel@gmail.com>
2023-03-23 02:14:58 +0800

    Update script.js
    Add on hover collapsing according

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 
+$(function(){
+  
+  $(".panel-default").hover(
+   function() {
+      $(this).find('.panel-collapse').collapse('show');
+    }, function() {
+      $(this).find('.panel-collapse').collapse('hide');
+    }
+  );
+  
+});
