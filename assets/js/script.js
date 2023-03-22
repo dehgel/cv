f20a4b77ab48d59212dd2b29fde48c00afd0f2cd
Deh Saaduddin <dehgel@gmail.com>
2023-03-23 02:01:12 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,1 @@
-$("body").live("click",function(){
 
-    if($("#Container div").position().top<-300)
-
-    	$("#Container div").animate({"top":"+=100px"});
-
-    else{
-
-        $("#Container div").animate({"top":"-=100px"});
-
-    }
-
-});
