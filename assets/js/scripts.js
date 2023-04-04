19f544ddaa90aff4f00e39f4444c00a570ea5a3c
Deh Saaduddin <dehgel@gmail.com>
2023-04-04 13:43:43 +0800

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,27 +29,6 @@ $(function() {
               
 });
 
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-function initClient() {
-  gapi.client.init({
-    apiKey: 'AIzaSyBKAN9Hqu2Ha3xr0V0bXq21tAJ0c2b-75Q',
-    clientId: 'YOUR_CLIENT_ID',
-    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
-    scope: 'https://www.googleapis.com/auth/gmail.readonly'
-  }).then(function() {
-    // Authorize the user
-    return gapi.auth2.getAuthInstance().signIn();
-  }).then(function() {
-    // The user is authorized, you can now use the Gmail API
-  }, function(error) {
-    console.log(error);
-  });
-}
-
-
 /* Scroll to Top button */
 $(document).ready(function() {
     var $scrollButton = $("#scrollTop");
@@ -66,27 +45,5 @@ $(document).ready(function() {
         $("html, body").animate({scrollTop: 0}, 600);
         return false;
     });
-
-    function sendEmail() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
-
-    var request = gapi.client.gmail.users.messages.send({
-      'userId': 'me',
-      'resource': {
-        'raw': btoa(
-          'To: ' + email + '\n' +
-          'Subject: ' + subject + '\n\n' +
-          message
-        )
-      }
-    });
-
-    request.execute(function(response) {
-      console.log(response);
-    });
-  }
 });
 
